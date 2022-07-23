f232a1b35b75418b669594bcbaf477af8366a6cf
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-07-23 16:14:05 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
- 
 import './App.css';
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <h1>Hello, world!</h1>
+    <div>
+      <Navbar/>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Navbar() {
+    return(
+    <nav className="navbar">
+        <h1 className="heading">Mrunal's  Travelogue</h1>
+    </nav>
+    )
+}
