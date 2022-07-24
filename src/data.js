d4a61db48e345797dcd2e1513977771f6a2b66a2
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-07-24 12:06:26 +0530

    added some more data

--- src/data.js
+++ src/data.js
@@ -1,5 +1,32 @@
 export default [
     {
+        title: "Manali",
+        location: "India",
+        googleMapsUrl: "https://goo.gl/maps/xSBusaPftNK7Z8qp6",
+        startDate: "12 Jan, 2020",
+        endDate: "24 Jan, 2020",
+        description: "Once called the 'end of the habitable world,' Manali is an important hill station of northern India and is the destination of thousands of tourists every year. Its cool atmosphereprovides a perfect haven for the ones afflicted by the hot Indian summers. Besides offering quite a few places for sightseeing, Manali is also famous for adventure sports like skiing, hiking, mountaineering, paragliding, rafting, trekking, kayaking, and mountain biking. ",
+        imageUrl: "https://www.travelxp.com/pages/wp-content/uploads/2022/01/manali-1.jpg"
+    },
+    {
+        title: "Venice",
+        location: "Italy",
+        googleMapsUrl: "https://goo.gl/maps/UzqG5P1nCy2scNTKA",
+        startDate: "12 Jun, 2020",
+        endDate: "24 July, 2020",
+        description: "Venice has no equals in all of Europe. In this city floating on a lagoon, streets are canals and crossings are bridges.Everything, from churches to palaces, speaks of more than a thousand years of history in which the Pearl of the Adriatic dominated the seas and grew wealthy on commerce. On top of that, there are world-famous art exhibitions, romantic gondolas, and mouth-watering aperitivos. No wonder Venice regularly tops the list of Italy's best places to visit. To help you plan your time in Venice, here are some of the city's top things to do.",
+        imageUrl: "https://cdn.britannica.com/63/153463-050-06B6270D/Grand-Canal-Venice.jpg"
+    },
+    {
+        title: "Fairmont Le Château Frontenac",
+        location: "Quebec",
+        googleMapsUrl: "https://goo.gl/maps/un9jBcqSaxRrbhtW9",
+        startDate: "12 Dec, 2020",
+        endDate: "24 Jan, 2022",
+        description: "The Fairmont Le Château Frontenac, commonly referred to as the Château Frontenac, is a historic hotel in Quebec City, Quebec, Canada. The hotel is situated in Old Quebec, within the historic district's Upper Town, on the southern side of Place d'Armes. The Château Frontenac was designed by Bruce Price, and was built by the Canadian Pacific Railway company. The hotel is managed by Fairmont Hotels and Resorts.",
+        imageUrl: "https://www.worldatlas.com/r/w1200/upload/0c/fd/49/shutterstock-428039830.jpg"
+    },
+    {
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -17,14 +44,6 @@ export default [
         description: "The N Seoul Tower officially the YTN Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Nam Mountain in central Seoul, South Korea. The 236-meter (774 ft)-tall tower marks the second highest point in Seoul",
         imageUrl: "https://cf.creatrip.com/original/spot/11239/%E5%8D%97%E5%B1%B1%E5%A1%94_Creatrip00.jpg"
     },
-    {
-        title: "Fairmont Le Château Frontenac",
-        location: "Quebec",
-        googleMapsUrl: "https://goo.gl/maps/un9jBcqSaxRrbhtW9",
-        startDate: "12 Dec, 2021",
-        endDate: "24 Jan, 2022",
-        description: "The Fairmont Le Château Frontenac, commonly referred to as the Château Frontenac, is a historic hotel in Quebec City, Quebec, Canada. The hotel is situated in Old Quebec, within the historic district's Upper Town, on the southern side of Place d'Armes. The Château Frontenac was designed by Bruce Price, and was built by the Canadian Pacific Railway company. The hotel is managed by Fairmont Hotels and Resorts.",
-        imageUrl: "https://www.worldatlas.com/r/w1200/upload/0c/fd/49/shutterstock-428039830.jpg"
-    }
+    
 
 ]
